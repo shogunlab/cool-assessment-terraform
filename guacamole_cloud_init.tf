8c96f539091bac01a068e906b75261f73985e794
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-09-27 01:50:55 -0400

    Add support for Windows to Guacamole cloud-init
    Add support for Windows instances to the Guacamole cloud-init configuration.
    This included adjusting the SQL template, the script to render the template,
    and the appropriate variables and documentation to support these changes.

--- guacamole_cloud_init.tf
+++ guacamole_cloud_init.tf
@@ -86,6 +86,8 @@ data "cloudinit_config" "guacamole_cloud_init_tasks" {
         guac_connection_setup_path       = var.guac_connection_setup_path
         instance_hostnames               = join(",", concat(aws_route53_record.assessorportal_A[*].name, aws_route53_record.debiandesktop_A[*].name, aws_route53_record.gophish_A[*].name, aws_route53_record.kali_A[*].name, aws_route53_record.pentestportal_A[*].name, aws_route53_record.teamserver_A[*].name, aws_route53_record.terraformer_A[*].name))
         ssm_vnc_read_role_arn            = aws_iam_role.vnc_parameterstorereadonly_role.arn
+        ssm_key_rdp_password             = var.ssm_key_rdp_password
+        ssm_key_rdp_user                 = var.ssm_key_rdp_username
         ssm_key_vnc_password             = var.ssm_key_vnc_password
         ssm_key_vnc_user                 = var.ssm_key_vnc_username
         ssm_key_vnc_user_private_ssh_key = var.ssm_key_vnc_user_private_ssh_key
